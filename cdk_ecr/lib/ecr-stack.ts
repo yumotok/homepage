d2c789699773b0c4b8e99c0e5e20fd8eae5b0fda
yumotok <66120060+yumotok@users.noreply.github.com>
2023-12-11 08:07:33 +0900

    resource-idを変更

--- cdk_ecr/lib/ecr-stack.ts
+++ cdk_ecr/lib/ecr-stack.ts
@@ -5,7 +5,7 @@ export class ECRStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     // ECRリポジトリの作成
-    new cdk.aws_ecr.CfnRepository(this, 'EcrRepository', {
+    new cdk.aws_ecr.CfnRepository(this, 'divide-prod', {
       repositoryName: 'divide-prod'
     });
   }
