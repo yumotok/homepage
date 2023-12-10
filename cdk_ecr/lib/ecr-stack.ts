43200bb604d0433e0f8322948dbbe3917a4da190
yumotok <66120060+yumotok@users.noreply.github.com>
2023-12-11 07:49:55 +0900

    ECRを追加

--- cdk_ecr/lib/ecr-stack.ts
+++ cdk_ecr/lib/ecr-stack.ts
@@ -4,5 +4,9 @@ import { Construct } from 'constructs';
 export class ECRStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    // ECRリポジトリの作成
+    new cdk.aws_ecr.CfnRepository(this, 'EcrRepository', {
+      repositoryName: 'divide-prod'
+    });
   }
 }
