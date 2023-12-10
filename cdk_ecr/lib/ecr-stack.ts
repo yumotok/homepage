9596ae13aebc4cfba2ca0ad32b6b361764f43fec
yumotok <66120060+yumotok@users.noreply.github.com>
2023-12-11 08:49:36 +0900

    exportを追加

--- cdk_ecr/lib/ecr-stack.ts
+++ cdk_ecr/lib/ecr-stack.ts
@@ -1,13 +1,18 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class ECRStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     // ECRリポジトリの作成
-    new cdk.aws_ecr.CfnRepository(this, 'divide-prod', {
-      repositoryName: 'divide-prod',
+    const ecr = new cdk.aws_ecr.CfnRepository(this, "divide-prod", {
+      repositoryName: "divide-prod",
       emptyOnDelete: true,
+    });
+
+    new cdk.CfnOutput(this, "ecr-repository-uri", {
+      value: ecr.attrRepositoryUri,
+      exportName: "ecr-repository-uri",
     });
   }
 }
