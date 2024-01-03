17145c089843bf9eb94ef2b092313c1e5d2a1b72
yumotok <66120060+yumotok@users.noreply.github.com>
2024-01-03 12:59:25 +0900

    ネットワークACLを構成に追加

--- cdk_base/lib/base-stack.ts
+++ cdk_base/lib/base-stack.ts
@@ -3,9 +3,12 @@ import {
   CfnEIP,
   CfnInternetGateway,
   CfnNatGateway,
+  CfnNetworkAcl,
+  CfnNetworkAclEntry,
   CfnRoute,
   CfnRouteTable,
   CfnSubnet,
+  CfnSubnetNetworkAclAssociation,
   CfnSubnetRouteTableAssociation,
   CfnVPC,
   CfnVPCGatewayAttachment,
@@ -33,72 +36,6 @@ class Resource<T extends CfnResource> {
   constructor(resource: T, resourceName: string) {
     this.raw = resource;
     this.resourceName = resourceName;
-  }
-}
-
-class RouteTableBuilder {
-  resourceName: string;
-  vpc: Resource<CfnVPC>;
-  subnets: Resource<CfnSubnet>[];
-  igw?: Resource<CfnInternetGateway>;
-  nat?: Resource<CfnNatGateway>;
-
-  constructor(init: {
-    resourceName: string;
-    vpc: Resource<CfnVPC>;
-    subnets: Resource<CfnSubnet>[];
-    igw?: Resource<CfnInternetGateway>;
-    nat?: Resource<CfnNatGateway>;
-  }) {
-    this.resourceName = init.resourceName;
-    this.vpc = init.vpc;
-    this.subnets = init.subnets;
-    this.igw = init.igw;
-    this.nat = init.nat;
-  }
-
-  make(scope: Construct, context: Context): Resource<CfnRouteTable> {
-    const table = new CfnRouteTable(
-      scope,
-      context.getResourceName(this.resourceName),
-      {
-        vpcId: this.vpc.raw.ref,
-        tags: [
-          { key: "Name", value: context.getResourceName(this.resourceName) },
-        ],
-      }
-    );
-
-    if (this.igw != null) {
-      new CfnRoute(
-        scope,
-        context.getResourceName(this.resourceName) + "-public-route",
-        {
-          routeTableId: table.ref,
-          destinationCidrBlock: "0.0.0.0/0",
-          gatewayId: this.igw.raw.ref,
-        }
-      );
-    } else if (this.nat != null) {
-      new CfnRoute(scope, context.getResourceName(this.resourceName) + "-nat", {
-        routeTableId: table.ref,
-        destinationCidrBlock: "0.0.0.0/0",
-        natGatewayId: this.nat.raw.ref,
-      });
-    }
-
-    this.subnets.forEach((subnet) => {
-      new CfnSubnetRouteTableAssociation(
-        scope,
-        subnet.resourceName + "-route-table-association",
-        {
-          routeTableId: table.ref,
-          subnetId: subnet.raw.ref,
-        }
-      );
-    });
-
-    return new Resource(table, context.getResourceName(this.resourceName));
   }
 }
 
@@ -160,27 +97,77 @@ export class BaseStack extends Stack {
 
     const tableBuilders = [
       new RouteTableBuilder({
-        resourceName: "public-route-table",
+        resourceName: "route-table-public",
         vpc: vpc,
         subnets: subnets.filter((s) => s.resourceName.includes("public")),
         igw: igw,
       }),
       new RouteTableBuilder({
-        resourceName: "app-route-table",
+        resourceName: "route-table-app",
         vpc: vpc,
         subnets: subnets.filter((s) => s.resourceName.includes("app")),
       }),
       new RouteTableBuilder({
-        resourceName: "private-route-table",
+        resourceName: "route-table-private",
         vpc: vpc,
         subnets: subnets.filter((s) => s.resourceName.includes("private")),
       }),
     ];
 
     // RouteTableを作成する
-    const routeTables = tableBuilders.map((builder) =>
-      builder.make(this, context)
+    tableBuilders.map((builder) =>
+      builder.run(this, context)
     );
+
+    const networkAclBuilders = [
+      new NetworkAclBuilder({
+        resourceName: "network-acl-public",
+        vpc: vpc,
+        subnetAssosiations: [
+          {
+            id: "network-acl-assosiation-public-subnet-1a",
+            subnet: subnets.find((s) => s.resourceName.includes("subnet-public-1a"))
+          },
+          {
+            id: "network-acl-assosiation-public-subnet-1c",
+            subnet: subnets.find((s) => s.resourceName.includes("subnet-public-1c"))
+          }
+        ]
+      }),
+      new NetworkAclBuilder({
+        resourceName: "network-acl-app",
+        vpc: vpc,
+        subnetAssosiations: [
+          {
+            id: "network-acl-assosiation-app-subnet-1a",
+            subnet: subnets.find((s) => s.resourceName.includes("subnet-app-1a"))
+          },
+          {
+            id: "network-acl-assosiation-app-subnet-1c",
+            subnet: subnets.find((s) => s.resourceName.includes("subnet-app-1c"))
+          }
+        ]
+      }),
+      new NetworkAclBuilder({
+        resourceName: "network-acl-private",
+        vpc: vpc,
+        subnetAssosiations: [
+          {
+            id: "network-acl-assosiation-private-subnet-1a",
+            subnet: subnets.find((s) => s.resourceName.includes("subnet-private-1a"))
+          },
+          {
+            id: "network-acl-assosiation-private-subnet-1c",
+            subnet: subnets.find((s) => s.resourceName.includes("subnet-private-1c"))
+          }
+        ]
+      }),
+    ];
+
+    // NetworkAclを作成する
+    networkAclBuilders.forEach((builder) => {
+      builder.run(this, context);
+    });
   }
 
   private createVpc(context: Context, resourceName: string): Resource<CfnVPC> {
@@ -222,5 +209,142 @@ export class BaseStack extends Stack {
     });
 
     return new Resource(internetGateway, context.getResourceName(resourceName));
+  }
+}
+
+class RouteTableBuilder {
+  resourceName: string;
+  vpc: Resource<CfnVPC>;
+  subnets: Resource<CfnSubnet>[];
+  igw?: Resource<CfnInternetGateway>;
+  nat?: Resource<CfnNatGateway>;
+
+  constructor(init: {
+    resourceName: string;
+    vpc: Resource<CfnVPC>;
+    subnets: Resource<CfnSubnet>[];
+    igw?: Resource<CfnInternetGateway>;
+    nat?: Resource<CfnNatGateway>;
+  }) {
+    this.resourceName = init.resourceName;
+    this.vpc = init.vpc;
+    this.subnets = init.subnets;
+    this.igw = init.igw;
+    this.nat = init.nat;
+  }
+
+  run(scope: Construct, context: Context): Resource<CfnRouteTable> {
+    const table = new CfnRouteTable(
+      scope,
+      context.getResourceName(this.resourceName),
+      {
+        vpcId: this.vpc.raw.ref,
+        tags: [
+          { key: "Name", value: context.getResourceName(this.resourceName) },
+        ],
+      }
+    );
+
+    if (this.igw != null) {
+      new CfnRoute(
+        scope,
+        context.getResourceName(this.resourceName) + "-public-route",
+        {
+          routeTableId: table.ref,
+          destinationCidrBlock: "0.0.0.0/0",
+          gatewayId: this.igw.raw.ref,
+        }
+      );
+    } else if (this.nat != null) {
+      new CfnRoute(scope, context.getResourceName(this.resourceName) + "-nat", {
+        routeTableId: table.ref,
+        destinationCidrBlock: "0.0.0.0/0",
+        natGatewayId: this.nat.raw.ref,
+      });
+    }
+
+    this.subnets.forEach((subnet) => {
+      new CfnSubnetRouteTableAssociation(
+        scope,
+        subnet.resourceName + "-route-table-association",
+        {
+          routeTableId: table.ref,
+          subnetId: subnet.raw.ref,
+        }
+      );
+    });
+
+    return new Resource(table, context.getResourceName(this.resourceName));
+  }
+}
+
+class NetworkAclBuilder {
+  resourceName: string;
+  vpc: Resource<CfnVPC>;
+  subnetAssosiations: {
+    id: string;
+    subnet?: Resource<CfnSubnet>;
+  }[];
+  constructor(init: {
+    resourceName: string;
+    vpc: Resource<CfnVPC>;
+    subnetAssosiations: {
+      id: string;
+      subnet?: Resource<CfnSubnet>;
+    }[];
+  }) {
+    this.resourceName = init.resourceName;
+    this.vpc = init.vpc;
+    this.subnetAssosiations = init.subnetAssosiations;
+  }
+
+  run(scope: Construct, context: Context): Resource<CfnNetworkAcl> {
+    const networkAcl = new CfnNetworkAcl(
+      scope,
+      context.getResourceName(this.resourceName),
+      {
+        vpcId: this.vpc.raw.ref,
+        tags: [
+          {
+            key: "Name",
+            value: context.getResourceName(this.resourceName),
+          },
+        ],
+      }
+    );
+
+    // Outboundエントリ
+    new CfnNetworkAclEntry(scope, context.getResourceName(this.resourceName)+"-oubound-entry", {
+      networkAclId: networkAcl.ref,
+      protocol: -1,
+      ruleAction: "allow",
+      ruleNumber: 100,
+      cidrBlock: "0.0.0.0/0",
+      egress: true,
+    });
+
+    // Inboundエントリ
+    new CfnNetworkAclEntry(scope, context.getResourceName(this.resourceName)+"-inbound-entry", {
+      networkAclId: networkAcl.ref,
+      protocol: -1,
+      ruleAction: "allow",
+      ruleNumber: 100,
+      cidrBlock: "0.0.0.0/0",
+      egress: false,
+    });
+
+    // Subnetとの関連付け
+    this.subnetAssosiations.forEach((assosiation) => {
+      if(assosiation.subnet == null){
+        throw new Error("subnet is null");
+      }
+
+      new CfnSubnetNetworkAclAssociation(scope,assosiation.id, {
+        networkAclId: networkAcl.ref,
+        subnetId: assosiation.subnet.raw.ref,
+      });
+    });
+
+    return new Resource(networkAcl, context.getResourceName(this.resourceName));
   }
 }
